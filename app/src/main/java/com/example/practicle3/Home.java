8d1ff4504b5236b2c935ba2595b036b49048fe59
master <bryngowi2@gmail.com>
2025-01-21 23:27:24 +0300

    v1

--- app/src/main/java/com/example/practicle3/Home.java
+++ app/src/main/java/com/example/practicle3/Home.java
@@ -1,16 +1,46 @@
 package com.example.practicle3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class Home extends Fragment {
+
+    Button review_btn, favorite_btn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize buttons
+        review_btn = view.findViewById(R.id.rev_btn);
+        favorite_btn = view.findViewById(R.id.fav_btn);
+
+        // Set click listeners for the buttons
+        review_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddReview.class);
+                startActivity(intent);
+            }
+        });
+
+        favorite_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddFavorite.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 }
